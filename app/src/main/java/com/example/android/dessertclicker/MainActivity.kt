730350a5789479326592ff7ccaa1a3a980568d7d
bancha <ronnybcjt2311@gmail.com>
2019-09-09 20:40:07 +0700

    Timber

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -79,6 +80,7 @@ class MainActivity : AppCompatActivity() {
         // Make sure the correct dessert is showing
         binding.dessertButton.setImageResource(currentDessert.imageId)
         Log.i("MainActivity", "onCreate Called")
+        Timber.i("onCreate called")
     }
 
 
@@ -148,8 +150,11 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
     override fun onStart() {
         super.onStart()
         Log.i("MainActivity", "onCreate Called")
+        Timber.i("onStart Called")
     }
+
 }
